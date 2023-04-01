db062ff468e7d2f2fe994c21f4b524c3839a6b71
widici <vidar.otto.johannessen@gmail.com>
2023-04-01 21:46:44 +0200

    Added UserData class and new function for UserData

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,24 @@ mod api;
 use crate::api::profile::{ProfileData, request_profile};
 use crate::api::repo::{RepoData, request_repos};
 
+struct UserData {
+    profile_data: ProfileData,
+    repo_data: RepoData,
+}
+
+impl UserData {
+    async fn new(username: &str) -> Result<UserData, reqwest::Error> {
+        let profile_data= request_profile(username).await?;
+        let repo_data = request_repos(username).await?;
+
+        return Ok( UserData { profile_data, repo_data } )
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let data: ProfileData = request_profile("widici").await?;
-    let field = data.bio;
+    let user_data: UserData = UserData::new("muhamed").await?;
+    let field = user_data.repo_data.language;
 
     if field.is_some() {
         println!("{}", field.unwrap());
